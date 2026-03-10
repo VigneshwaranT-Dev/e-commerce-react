7835faa23608f57a46e6baed6a189dad6b02e2c5
Vigneshwaran Thiruselvam <vigneshwarant54@gmail.com>
2026-03-10 22:44:39 +0530

    initial react project setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-gray-100">
+        <header className="bg-white shadow p-4">
+          <h1 className="text-2xl font-bold text-blue-600">
+            Electronics Store
+          </h1>
+        </header>
+
+        <main className="p-10">
+          <h2 className="text-xl font-semibold">Featured Products</h2>
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
